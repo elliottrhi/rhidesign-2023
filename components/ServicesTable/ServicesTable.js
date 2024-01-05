be1e39d9638fd7bbc2259c0f42df3c13ac81494e
elliottrhi <elliottrhi@gmail.com>
2024-01-04 23:14:26 -1000

    Modified CSS.

--- components/ServicesTable/ServicesTable.js
+++ components/ServicesTable/ServicesTable.js
@@ -41,7 +41,7 @@ export const ServicesTable = () => {
             <div className="mx-auto flex">
                 <span className="mx-auto text-center text-[22px]">Learn more about what I do <svg id="details-arrow" xmlns="http://www.w3.org/2000/svg" height="16" width="12" viewBox="0 0 384 512"><path fill="currentColor" d="M32 64C14.3 64 0 49.7 0 32S14.3 0 32 0l96 0c53 0 96 43 96 96l0 306.7 73.4-73.4c12.5-12.5 32.8-12.5 45.3 0s12.5 32.8 0 45.3l-128 128c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 402.7 160 96c0-17.7-14.3-32-32-32L32 64z"/></svg></span>
             </div>
-            <div className="mx-auto grid grid-row-1">
+            <div className="mx-auto grid grid-row-1 cursor-pointer">
                 {accordionData.map((data, index) => { return (
                     <ServicesAccordion 
                     key={index} 
